36338bb9e0d6bcf18c940aff2c28d6421084870d
Adams Zago <adams.zago@gmail.com>
2019-07-26 17:57:12 -0300

    Putting pieces King and Rook on the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -36,4 +36,9 @@ public class Board {
 		return pieces[pos.getRow()][pos.getColumn()];
 	}
 	
+	public void placePiece(Piece piece, Position position) {
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
+	}
+	
 }
